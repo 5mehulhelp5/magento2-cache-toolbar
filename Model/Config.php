0ddfc0d99ea014264c78117dda1908af04dc2183
Max Pronko <max.pronko@gmail.com>
2026-05-18 08:52:55 +0100

    Fixed config namespace

--- Model/Config.php
+++ Model/Config.php
@@ -11,10 +11,10 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Config
 {
-    private const XML_PATH_ENABLED     = 'cache_toolbar/general/enabled';
-    private const XML_PATH_SHORTCUT    = 'cache_toolbar/general/shortcut_enabled';
-    private const XML_PATH_SMART_TYPES = 'cache_toolbar/smart_clear/types';
-    private const XML_PATH_PROMO       = 'cache_toolbar/promo/enabled';
+    private const XML_PATH_ENABLED     = 'pronko_cache_toolbar/general/enabled';
+    private const XML_PATH_SHORTCUT    = 'pronko_cache_toolbar/general/shortcut_enabled';
+    private const XML_PATH_SMART_TYPES = 'pronko_cache_toolbar/smart_clear/types';
+    private const XML_PATH_PROMO       = 'pronko_cache_toolbar/promo/enabled';
 
     public function __construct(
         private readonly ScopeConfigInterface $scopeConfig
